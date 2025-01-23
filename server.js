d5791f2675650b768b32122887f192a64975d58e
Divya Dadhich <divya.dadhich.s63@kalvium.community>
2025-01-23 18:18:24 +0530

    Resolved hardcoded port issue

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.get('/ping', (req, res) => {
     res.send('pong');
     });
@@ -9,3 +9,6 @@ app.get('/ping', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     });
+
+
+    
